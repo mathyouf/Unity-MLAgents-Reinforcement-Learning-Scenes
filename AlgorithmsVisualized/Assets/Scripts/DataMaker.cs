0681d1983ae273c380a4a83c63b8a6a517d4a88d
Matthew Fisher <11fishem@gmail.com>
2020-08-20 13:15:15 +0900

    start

---
+++ AlgorithmsVisualized/Assets/Scripts/DataMaker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Scripting.Python;
+using UnityEditor;
+using System.IO;
+
+public class DataMaker
+{
+    [MenuItem("Python/mergeSort")]
+    static void MergeSort()
+    {
+        string scriptPath = Path.Combine(Application.dataPath, "MergeSort.py");
+        PythonRunner.RunFile(scriptPath);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Unity_MLAgents-ReinforcementLearning
+# Projects using Unity with ML Agents
+
 Reinforcement Learning environments made with Unity, with agents trained using MLAgents. 3D environments with gravity and rigid bodies.
+
+![bball](Images/bball.PNG)
+![tensorboard](Images/tb.png)
+![soccer](Images/AgentsWolf.PNG)
